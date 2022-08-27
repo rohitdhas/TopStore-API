dbf91b263c481b1e35e224c572dc291926191960
Rohit Dhas <rohitdhas666@gmail.com>
2022-08-27 20:32:02 +0530

    port update

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ mongoose
     useUnifiedTopology: true,
   })
   .then(() => {
-    app.listen(4000, (err) => {
+    app.listen(process.env.PORT || 4000, (err) => {
       if (err) console.log(err);
       else console.log("Server is running!");
     });
