41a8beb9b25e0e04b45f0f3393aa108be4cef285
Rohit Dhas <rohitdhas666@gmail.com>
2022-08-27 20:11:45 +0530

    configured to deploy on vercel

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,13 @@
-import './styles/App.css';
-import Nav from './Components/nav';
-import Home from './Components/home';
-import Docs from './Components/docs';
-import PageNotFound from './Components/404'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import "./styles/App.css";
+import Nav from "./Components/nav";
+import Home from "./Components/home";
+import Docs from "./Components/docs";
+import PageNotFound from "./Components/404";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <Router>
+    <Router basename="/">
       <Nav />
       <Switch>
         <Route exact path="/">
--- server/server.js
+++ server/server.js
@@ -1,42 +1,46 @@
 // ________________________ IMPORTS ________________________
-require('dotenv').config();
-const mongoose = require('mongoose');
-const cors = require('cors');
-const express = require('express');
+require("dotenv").config();
+const mongoose = require("mongoose");
+const cors = require("cors");
+const express = require("express");
 const app = express();
-const productRoutes = require('./Routes/product_routes');
+const productRoutes = require("./Routes/product_routes");
 
 // ________________________ DATABASE CONNECTION ________________________
 
-mongoose.connect(process.env.MONGO_URI, {
+mongoose
+  .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-})
-    .then(() => {
-        app.listen(4000, (err) => {
-            if (err) console.log(err);
-            else console.log('Server is running!');
-        })
-        console.log("Mongoose Connected!✅")
-    })
-    .catch(() => console.log("Its error from DB!"))
-
+  })
+  .then(() => {
+    app.listen(4000, (err) => {
+      if (err) console.log(err);
+      else console.log("Server is running!");
+    });
+    console.log("Mongoose Connected!✅");
+  })
+  .catch(() => console.log("Its error from DB!"));
 
 // ________________________ MIDDLEWARES ________________________
 
 app.use(express.json());
 
-app.use(cors({
-    origin: "*"
-}))
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 
 // ________________________ ROUTES ________________________
 
-app.get('/', (req, res) => {
-    res.send("Welcome to TopStore API!");
-})
+app.use("/products", productRoutes);
 
-app.use('/products', productRoutes);
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client", "build")));
+  app.get("/*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../client", "build", "index.html"));
+  });
+}
 
 // ________________________________________________________
-
